599bc7e310703a9c474f9639a7dccae4086dcaac
PC1 <kevsalomdev@gmail.com>
2023-11-17 12:24:55 -0400

    first commit

---
+++ src/app/api/tasks/[id]/route.js
@@ -0,0 +1,26 @@
+import { NextResponse } from "next/server";
+import prisma from "@/app/Libs/prisma";
+
+export async function GET(request,{params}){
+    const task = await prisma.task.findUnique({where:{
+        id: Number(params.id)
+    }})
+    return NextResponse.json(task)
+}
+
+export async function PUT(request,{params}){
+    const data = await request.json()
+    const task = await prisma.task.update({where:{
+        id:Number(params.id)
+    }, data})
+    return NextResponse.json(task)
+}
+
+
+export async function DELETE(request,{params}){
+    await prisma.task.delete({where:{
+        id:Number(params.id)
+    }})
+    return NextResponse.json({message: 'eliminado correctamente'})
+}
+
---
+++ src/app/api/tasks/route.js
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server";
+import prisma from "@/app/Libs/prisma";
+
+
+export async function GET() {
+  const data = await prisma.task.findMany();
+  return NextResponse.json(data);
+}
+
+export async function POST(request) {
+    const {title, description} = await request.json()
+    const newTask = await prisma.task.create({data:{
+        title, description
+    }})
+  return NextResponse.json(newTask);
+}
