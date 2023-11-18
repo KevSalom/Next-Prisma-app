0e66e888ff0e4bc4bf060c7100738462160d4050
PC1 <kevsalomdev@gmail.com>
2023-11-18 00:03:35 -0400

    get task, edit task

--- src/app/new/page.jsx
+++ src/app/new/page.jsx
@@ -1,27 +1,60 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
-const NewPage = () => {
+const NewPage = ({ params }) => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
 
-  const router = useRouter()
-  const handleSubmit = async(e) => {
-   e.preventDefault()
-   try {
-    const dataTask = {title:e.target.title.value, description: e.target.description.value}
-   const response = await fetch('/api/tasks', {
-    method:'POST',
-    body: JSON.stringify(dataTask),
-    headers:{
-      'Content-Type':'application/json'
+  useEffect(() => {
+    if(params.id) {
+      fetch(`/api/tasks/${params.id}`)
+        .then((data) => data.json())
+        .then(({ title, description }) => {
+          setTitle(title);
+          setDescription(description);
+        }).catch((error)=>console.error(error))
     }
-   })
-   e.target.reset()
-  router.push('/')
-   } catch (error) {
-     console.error(error.message)
-   }
-   
+  }, []);
+
+  const router = useRouter();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const dataTask = {
+        title,
+        description,
+      };
+
+      if(!params.id){
+        await fetch("/api/tasks", {
+          method: "POST",
+          body: JSON.stringify(dataTask),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+      } else{
+        await fetch(`/api/tasks/${params.id}`, {
+          method: 'PUT',
+          body: JSON.stringify(dataTask),
+          headers: {
+            'Content-Type': 'app`lication/json'
+          },
+        })
+      router.refresh()
+      }
+      
+
+      router.push("/");
+    } catch (error) {
+      console.error(error.message);
+    }
+
+
+
   };
   return (
     <div className="h-screen flex justify-center items-center ">
@@ -30,18 +63,35 @@ const NewPage = () => {
         className="flex flex-col bg-slate-500 p-4 w-full sm:w-1/4 gap-4 rounded-md"
       >
         <div className="flex flex-col gap-1 ">
-          <label htmlFor="title" >Titulo</label>
-          <input type="text" id="title" name="title"  className="rounded-sm  text-black p-1"/>
+          <label htmlFor="title">Título</label>
+          <input
+            type="text"
+            id="title"
+            name="title"
+            className="rounded-sm  text-black p-1"
+            placeholder="Escribe título..."
+            onChange={(e)=> setTitle(e.target.value)}
+            value={title}
+          />
         </div>
 
         <div className="flex flex-col gap-1">
-          <label htmlFor="description" >Descripción</label>
-          <textarea name="description" id="description" rows="3" className="rounded-sm text-black  p-1"></textarea>
+          <label htmlFor="description">Descripción</label>
+          <textarea
+            name="description"
+            id="description"
+            rows="3"
+            className="rounded-sm text-black  p-1"
+            placeholder="Escribe una descripción..."
+            onChange={(e)=> setDescription(e.target.value)}
+            value={description}
+          ></textarea>
         </div>
-        <button className="bg-slate-800 py-2 rounded-sm">Crear Tarea</button>
+        <button className="bg-slate-800 py-2 rounded-sm">{(params.id)?'Actualizar tarea':'Crear Tarea'}</button>
       </form>
     </div>
   );
 };
 
 export default NewPage;
+export const dynamic = 'force-dynamic';
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,17 +1,20 @@
 import TaskCard from "@/Components/TaskCard"
+
 const getData = async()=>{
   const data = await fetch('http://localhost:3000/api/tasks')
-  return await data.json()
+  const result = await data.json()
+  return  result
 }
-const HomePage = async() => {
+async function HomePage(){
 
   const tasks = await getData()
 
   return (
     <section className=" container mx-auto grid-cols-2 grid md:grid-cols-3 gap-3 mt-10">
-      {tasks.map(task =><TaskCard task={task} key={task.id} />)}
+      {tasks.map(task =><TaskCard key={task.id} task={task} />)}
     </section>
   )
 }
 
 export default HomePage
+export const dynamic = 'force-dynamic';
